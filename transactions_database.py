7e8b36f5baeccd71ad687b2937c0ee21c1f1ff63
neesarg123 <60748167+neesarg123@users.noreply.github.com>
2020-03-10 02:49:10 -0500

    Update transactions_database.py

--- transactions_database.py
+++ transactions_database.py
@@ -1,31 +1,32 @@
+# The point of this file is to store the transactions as they occur in the transaction mode,
+# and the whole table will be sent off to all_transactions database to keep track of all transactions.
+# Once that occurs, the data in this table will be deleted to be populated once again when a new transaction
+# event begins.
 import sqlite3
 
 conn = sqlite3.connect('transactions.db')
 
 c = conn.cursor()
 
+# creating a database table at first run
 c.execute("""CREATE TABLE IF NOT EXISTS transactions
 (name text, sp real, date text)""")
 
 
-def addData(itemName, salePrice, date):
-    params = (itemName, salePrice, date)
+def addData(item_name, sale_price, date):
+    params = (item_name, sale_price, date)
     c.execute("INSERT INTO transactions VALUES (?, ?, ?)", params)
     conn.commit()
-    print("Transaction was added!")
 
 
 def getData():
     c.execute("SELECT * FROM transactions")
     rows = c.fetchall()
-    # print("NAME/BARCODE/PURCHASE PRICE/SALE PRICE/QUANTITY")
-
     return list(rows)
 
 
 def deleteData():
     c.execute("DELETE FROM transactions")
-    print("Data was deleted!")
 
 
 conn.commit()
